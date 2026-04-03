2cbe7e6f281e9235806a25485e57c54692c3921d
Vaibhavsh0120 <vaibhavsh0120@gmail.com>
2026-04-03 10:40:12 +0530

    Fix Vercel Flutter web build setup

---
+++ vercel-install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+FLUTTER_VERSION=3.29.3
+FLUTTER_REPO=https://github.com/flutter/flutter.git
+
+if [ -d flutter ]; then
+  cd flutter
+  git fetch --depth 1 origin "$FLUTTER_VERSION"
+  git checkout "$FLUTTER_VERSION"
+  cd ..
+else
+  git clone --depth 1 --branch "$FLUTTER_VERSION" "$FLUTTER_REPO" flutter
+fi
+
+./flutter/bin/flutter config --enable-web
+./flutter/bin/flutter pub get
